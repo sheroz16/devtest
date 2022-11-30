f8b14de7c21e0c532810e539707367ab869812b8
= <=>
2022-11-30 15:34:56 +0530

    hello world

---
+++ src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping("/home")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/bye")
+	public String bye() {
+		return "Bye World";
+	}
+}
